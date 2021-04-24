d3223e038fcd5587cb68adc0c0434271594fe250
Diego Araujo <diego_shakan@hotmail.com>
2021-04-24 18:44:35 -0300

    another try

--- algorithms/maximize_array.rb
+++ algorithms/maximize_array.rb
@@ -1,8 +1,12 @@
 lista = [9, 8, 8, 5]
 k = 3
-soma = 0
 
-for j in 0..k-1
-  lista[j] = lista[j] * (-1)
+for i in 0..k-1
+  if lista[i] < 0
+    lista[i] = lista[i] * (-1)
+  end
 end
+
 puts lista.sum
+
+
